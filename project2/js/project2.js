d5d2bf6b4017c0158173cfa8d6bf22cfea6abace
tarosjk <tarosjk@gmail.com>
2024-06-08 12:01:19 +0900

    Project2完成

--- project2/js/project2.js
+++ project2/js/project2.js
@@ -5,8 +5,20 @@
   const form = document.querySelector('#contact-form')
   const requestUrl = 'https://zipcloud.ibsnet.co.jp/api/search?zipcode='
   const request = new XMLHttpRequest
+  const zippattern = /^[0-9-]{7,8}$/
 
   btn.addEventListener('click', () => {
+    // 郵便番号が入力されていない場合
+    if (form.zipcode.value === '') {
+      alert('郵便番号が入力されていません')
+      return
+    }
+
+    if (!zippattern.test(form.zipcode.value)) {
+      alert('郵便番号のフォーマットが違います')
+      return
+    }
+
     const url = requestUrl + form.zipcode.value
 
     request.open('GET', url)
@@ -14,6 +26,12 @@
     request.onload = () => {
       const address = JSON.parse(request.responseText)
       console.log(address)
+
+      if (!address.results) {
+        alert('住所が見つかりませんでした')
+        return
+      }
+
       console.log(address.results[0].address1)
 
       setAddress(address)
